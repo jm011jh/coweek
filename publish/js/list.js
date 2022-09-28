ce5d76ebb19b8e428937876c4a7bd85d157cfde4
jm011jh <jm011jh@gmail.com>
2022-09-28 19:36:00 +0900

    매무리

--- publish/js/list.js
+++ publish/js/list.js
@@ -1,8 +1,12 @@
 const listSortOpenClass = document.getElementById("listSortOpenClass")
 const listSortClassPop = document.getElementById("listSortClassPop")
+const listSortClassClose = document.getElementById("listSortClassClose")
+const listSortClassReset = document.getElementById("listSortClassReset")
 
 const listSortOpenWhere = document.getElementById("listSortOpenWhere")
 const listSortWherePop = document.getElementById("listSortWherePop")
+const listSortWhereClose = document.getElementById("listSortWhereClose")
+const listSortWhereReset = document.getElementById("listSortWhereReset")
 
 listSortOpenClass.addEventListener("click",function(){
     if(!listSortOpenClass.classList.contains("open")){
@@ -27,7 +31,14 @@ listSortOpenWhere.addEventListener("click",function(){
         listSortWherePop.classList.remove("open")
     }
 })
-
+listSortClassClose.addEventListener("click",function(){
+    listSortOpenClass.classList.remove("open")
+    listSortClassPop.classList.remove("open")
+})
+listSortWhereClose.addEventListener("click",function(){
+    listSortOpenWhere.classList.remove("open")
+    listSortWherePop.classList.remove("open")
+})
 const selectItemDummy = document.querySelectorAll(".list--head-sort-pop-selectItem")
 selectItemDummy.forEach(function(el,i){
     el.addEventListener("click",function(){
@@ -37,4 +48,32 @@ selectItemDummy.forEach(function(el,i){
             el.classList.remove("checked")
         }
     })
+})
+listSortClassReset.addEventListener("click",function(){
+    for(item of selectItemDummy){
+        item.classList.remove("checked")
+    }
+})
+listSortWhereReset.addEventListener("click",function(){
+    for(item of selectItemDummy){
+        item.classList.remove("checked")
+    }
+})
+
+const fieldItem = document.querySelectorAll('.list--head-sort-fieldItem')
+fieldItem.forEach(function(el,i){
+    el.addEventListener("click",function(){
+        for(item of fieldItem){
+            item.classList.remove("on")
+        }
+        el.classList.add("on")
+    })
+})
+document.querySelector(".list--head-sort-reset").addEventListener('click',function(){
+    for(item of fieldItem){
+        item.classList.remove("on")
+    }
+    for(item of selectItemDummy){
+        item.classList.remove("checked")
+    }
 })
